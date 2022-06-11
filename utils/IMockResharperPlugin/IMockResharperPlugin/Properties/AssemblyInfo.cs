080ffbc051345fd9eef239a0285339963e9d1d3a
Quinn Gil <Fyzxs@users.noreply.github.com>
2022-06-11 01:17:40 -0700

    Update to latest resharper

--- utils/IMockResharperPlugin/IMockResharperPlugin/Properties/AssemblyInfo.cs
+++ utils/IMockResharperPlugin/IMockResharperPlugin/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MicroObjectMocksResharperPlugin")]
-[assembly: AssemblyCopyright("Copyright © Fyzxs 2019")]
+[assembly: AssemblyCopyright("Copyright © Fyzxs 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
